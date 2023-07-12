9f3d87b8ef84fb3ed4d26e14f2e259f6aad27794
Antoine Racine <antoine.r@getjobber.com>
2023-07-12 12:21:56 -0400

    convenience with nvim-tree

--- lua/user/nvim2tree.lua
+++ lua/user/nvim2tree.lua
@@ -3,4 +3,22 @@ vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
   sort_by = "name",
+  update_focused_file = {
+    enable = true,
+    ignore_list = {},
+  },
+  actions = {
+    open_file = {
+      quit_on_open = false,
+      resize_window = true,
+      window_picker = {
+        enable = false,
+        picker = "default",
+        exclude = {
+          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+          buftype = { "nofile", "terminal", "help" },
+        },
+      },
+    },
+  },
 })
