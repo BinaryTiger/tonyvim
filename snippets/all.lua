c69ce6f004a634f4c347ae05ccbdf69e49b0b2ad
Antoine Racine <racine.antoine@gmail.com>
2024-01-08 15:51:58 -0500

    add general purpose snippets

--- snippets/all.lua
+++ snippets/all.lua
@@ -1,16 +1,18 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+
 return {
-  -- Example: how to set snippet parameters
-  require("luasnip").snippet(
-    { -- Table 1: snippet parameters
-      trig="hi_test",
-      dscr="An autotriggering snippet that expands 'hi' into 'Hello, world!'",
-      regTrig=false,
-      priority=100,
-      snippetType="autosnippet"
-    },
-    { -- Table 2: snippet nodes (don't worry about this for now---we'll cover nodes shortly)
-      t("Hello, world!"), -- A single text node
+  s({trig="today", dscr="Today's date, iso8601"},
+    {
+      t(os.date("%Y-%m-%d"))
     }
-    -- Table 3, the advanced snippet options, is left blank.
   ),
 }
