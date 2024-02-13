c062f425257443bf56ac22f5f05df150007e19c6
Antoine Racine <racine.antoine@gmail.com>
2024-02-13 13:28:11 -0500

    remove day productivity loop (live in journal now) and automate the blocks to be 30min long

--- lua/user/helpers.lua
+++ lua/user/helpers.lua
@@ -8,8 +8,9 @@ function helpers.today(_, _, _)
   return os.date("%Y-%m-%d")
 end
 
-function helpers.now(_, _, _)
-  return os.date("%H:%M")
+function helpers.now(_, _, time_offset)
+  -- offset is in seconds
+  return os.date("%H:%M", os.time() + time_offset)
 end
 
 return helpers
